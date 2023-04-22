07de82efb44accc37323707c8dea4d2696b63263
Shiva953 <b22070@students.iitmandi.ac.in>
2023-04-22 16:52:57 +0530

    added multiple news postings for a particular topic

--- commands/fun/techsource.js
+++ commands/fun/techsource.js
@@ -15,8 +15,36 @@ module.exports = {
         const response = await axios.get(`https://newsdata.io/api/1/news?apikey=${NEWSDATA_API_KEY}&q=${topic}&language=en&category=technology`);
         const tech_news = response.data;
       
-        const links = tech_news.results.map(result => result.link).join('\n');
-        await interaction.reply(`Here's the latest technology news for ${topic} in the last 48 hours:\n${links}`);
+        // Check if there are any news articles
+  if (tech_news.results.length === 0) {
+    await interaction.reply(`Sorry, there are no news articles found for ${topic}.`);
+    return;
+  }
+
+  // Create a new embed message
+  const embed = {
+    color: parseInt('0099ff', 16),
+    title: `Latest technology news for ${topic}`,
+    description: '',
+    thumbnail: { url: tech_news.results[0].image_url },
+    fields: [],
+    footer: {
+      text: 'Powered by NewsData.io',
+      icon_url: 'https://www.newsdata.io/static/images/Logo-Blue.svg',
+    },
+  };
+
+  // Loop through each news article and add a field to the embed message
+  for (let i = 0; i < tech_news.results.length; i++) {
+    const article = tech_news.results[i];
+    embed.fields.push({
+      name: article.title,
+      value: `${article.description}\n[Read more](${article.link})\n\n`
+    });
+  }
+
+  // Send the embed message
+  await interaction.reply({ embeds: [embed] });
+}
       }
       
-  };
