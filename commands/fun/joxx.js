31b143577bfbafb61d488b036f497d39adf8bd9e
Shiva953 <b22070@students.iitmandi.ac.in>
2023-04-22 20:51:57 +0530

    added joxx

---
+++ commands/fun/joxx.js
@@ -0,0 +1,38 @@
+require('dotenv').config
+const { SlashCommandBuilder } = require('discord.js')
+const axios = require('axios');
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('joxx')
+        .setDescription(`Tell me a Joke!`)
+        .addStringOption(option =>
+            option
+            	.setName('category')
+                .setDescription('Which category?(Programming/Dark/Misc/Pun/Spooky/Christmas')
+                .setRequired(true)
+        ),
+    async execute(interaction) {
+        const category = interaction.options.getString('category')
+        const response = await axios.get(`https://v2.jokeapi.dev/joke/${category}`);
+        const joke = response.data
+        try{
+            if(joke.error===true){
+                await interaction.reply(`Enter valid category!`)
+            }
+            else{
+            if(joke.type==="twopart"){
+                await interaction.reply(`${joke.setup}
+${joke.delivery}`);
+            }
+            else{
+            await interaction.reply(`${joke.joke}`)
+            }
+        }
+        }
+        catch(error){
+            console.log(error)
+            await interaction.reply("There was an error while executing this command! Please check the category")
+        }
+
+    }
+}
