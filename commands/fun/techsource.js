13cf95ea9c66006ee93891d6fa210d580a897285
Shiva953 <b22070@students.iitmandi.ac.in>
2023-04-27 13:06:03 +0530

    added category

--- commands/fun/techsource.js
+++ commands/fun/techsource.js
@@ -6,18 +6,28 @@ const NEWSDATA_API_KEY = process.env.NEWSDATA_API_KEY
 module.exports = {
 	cooldown : 4,
 	data: new SlashCommandBuilder()
-	  .setName('technews')
-	  .setDescription('Get the latest technology news!')
-	  .addStringOption(option => option.setName('topic').setDescription('The topic to get tech news for').setRequired(true)),
+	  .setName('news')
+	  .setDescription('Get the latest news!')
+    .addStringOption(option => option.setName('category').setDescription('Which Category?(Tech/Finance/Sports/etc.)').setRequired(true).addChoices(
+      { name: 'science', value: 'science' },
+      { name: 'technology', value: 'technology' },
+      { name: 'business', value: 'business' },
+      { name: 'environment', value: 'environment' },
+      { name: 'health', value: 'health' },
+      { name: 'food', value: 'food' },
+      { name: 'sports', value: 'sports' },
+    ))
+	  .addStringOption(option => option.setName('topic').setDescription('The topic!').setRequired(true)),
 
       async execute(interaction) {
         await interaction.deferReply(`The Bot is Thinking...`)
         const topic = interaction.options.getString('topic');
-        const response = await axios.get(`https://newsdata.io/api/1/news?apikey=${NEWSDATA_API_KEY}&q=${topic}&language=en&category=technology`);
-        const tech_news = response.data;
+        const category = interaction.options.getString('category');
+        const response = await axios.get(`https://newsdata.io/api/1/news?apikey=${NEWSDATA_API_KEY}&q=${topic}&language=en&category=${category}`);
+        const news = response.data;
       
         // Check if there are any news articles
-  if (tech_news.results.length === 0) {
+  if (news.results.length === 0) {
     await interaction.editReply(`Sorry, there are no news articles found for ${topic}.`);
     return;
   }
@@ -25,7 +35,7 @@ module.exports = {
   // Create a new embed message
   const embed = {
     color: parseInt(Math.floor(Math.random() * 16777215).toString(16),16),
-    title: `Latest technology news for ${topic}`,
+    title: `Latest ${category} news for ${topic}`,
     description: '',
     thumbnail: { url: '' },
     fields: [],
@@ -36,8 +46,8 @@ module.exports = {
   };
 
   // Loop through each news article and add a field to the embed message
-  for (let i = 0; i < tech_news.results.length; i++) {
-    const article = tech_news.results[i];
+  for (let i = 0; i < news.results.length; i++) {
+    const article = news.results[i];
     embed.fields.push({
       name: article.title,
       value: `${article.description}\n[Read more](${article.link})\n\n`
